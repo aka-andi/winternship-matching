15db3bd84b0923df63d6998078f4e934f0e54bb5
zahiniweeni <zahinfaruque@gmail.com>
2021-10-22 13:17:48 -0400

    Added matching algorithm

--- src/entity_classes.py
+++ src/entity_classes.py
@@ -1,90 +1,198 @@
+import math
+import json
+from statistics import stdev
+from math import floor
+
+SCORING_CRITERIA = json.load(open("scoring_config.json"))
+SENTIMENT_TO_VALUE = {
+    "strongly disagree" : 1,
+    "somewhat disagree": 2,
+    "neither agree nor disagree": 3,
+    "somewhat agree": 4,
+    "strongly agree": 5,
+    "not at all likely": 1,
+    "a little likely": 2,
+    "somewhat likely": 3,
+    "likely": 4,
+    "very likely": 5
+}
+
 class Project:
 
-    def __init__(self, data, id):
-        self.id = id
+    def __init__(self, preference_data=None, capacity=30):
+        if preference_data == None:
+            self.organization = ""
+            self.preferred_students = []
+            self.excluded_students = []
+        else:
+            self.organization = preference_data['Organization']
+            self.preferred_students = preference_data['Prefer'].split(" / ")
+            for i in range(len(self.preferred_students)):
+                self.preferred_students[i] = self.preferred_students[i].upper()
+            self.excluded_students = preference_data['Exclude'].split(" / ")
+        self.capacity = capacity
+        self.supports_f1j1 = False
+        self.student_matches = []
 
-        # Project information
-        self.organization = data['organization']
-        self.location = data['location'] # GPS coordinates
-        self.remote = data['remote'] # True or false
+    def add_match(self, student):
+        if len(self.student_matches) + 1 <= self.capacity:
+            self.student_matches.append(student.legal_name)
+            self.student_matches = list(set(self.student_matches))
 
-        # Project constraints
-        self.max_students = data['constraints']['max-students']
-        self.major_constraints= data['constraints']["majors"]
-        self.skillset_constraints = data['constraints']["skillsets"]
-        self.class_constraints = data['constraints']["classes"]
-        self.school_constraints = data['constraints']["schools"]
-        self.gpa_constraints = data['constraints']["min-GPA"]
+    def add_project_info(self, project_info_data):
+        self.capacity = int(project_info_data['Number of Students'].strip())
+        self.supports_f1j1 = True if project_info_data['F1/J1'].strip() == "True" else False
 
-        # Match results
-        self.current_student_ids = []
-        self.eligible_student_ids = []
 
-    def major_ok(self, major):
+    def diversity_score(self, students):
+        '''
+        A measure of how diverse the current set of students is. Can and
+        should be modified to include more criteria.
+        '''
+        min_comfort_level = int(SCORING_CRITERIA["DIVERSITY-SCORING"]["overall_comfort_level"]["min"])
+        max_comfort_level = int(SCORING_CRITERIA["DIVERSITY-SCORING"]["overall_comfort_level"]["max"])
+        comfort_levels = [student.overall_comfort_level for student in students if student.legal_name in self.student_matches]
+        frequencies = [comfort_levels.count(x) for x in range(min_comfort_level, max_comfort_level + 1)]
+        return stdev(frequencies) / (stdev([self.capacity] + ([0] * (max_comfort_level - min_comfort_level))))
+
+    def reached_capacity(self):
+        return len(self.student_matches) == self.capacity
+
+    def student_eligible(self, student):
+        if student.legal_name in self.excluded_students:
+            return False
+        if (('F1' in student.visa_status or 'J1' in student.visa_status) and not self.supports_f1j1):
+            return False
         return True
-
-    def skillset_ok(self, skillset):
-        return True
-
-    def year_ok(self, year):
-        # year = (1, 2, 3 or 4) = (freshman, sophomore, junior, senior)
-        return True
-
-    def school_ok(self, school):
-        return True
-
-    def gpa_ok(self, GPA):
-        return True
-
-    def get_eligible(self, students):
-        for student in students:
-            evaluation = [
-                self.major_ok(student.major),
-                self.skillset_ok(student.skillset),
-                self.year_ok(student.year),
-                self.school_ok(student.school),
-                self.gpa_ok(student.GPA)
-            ]
-            if evaluation == [True, True, True, True, True]:
-                self.eligible_student_ids.append(student.id)
 
 class Student:
 
-    def __init__(self, data, id):
+    def __init__(self, id):
         self.id = id
 
-        # Student information
-        self.name = data['name']
-        self.school = data['school']
-        self.major = data['major']
-        self.year = data['year']
-        self.gpa = data['GPA']
-        self.highest_math = data['highest_math']
+        # Student application information
+        self.legal_name =  ""
+        self.gnumber = ""
+        self.degree = ""
+        self.credits_range = (0, 120) # (min, max)
+        self.graduation_year = ""
+        self.majors = []
+        self.gpa_range = (2.0, 3.0) # (min, max)
+        self.num_cs_courses = 0
+        self.experience = False
+        self.residency_status = ""
+        self.commitment = False
+        self.eligibility = False
 
-        # Student constraints
-        self.location_constraints = data['constraints']['location']
-        self.pay_constraints = data['constraints']['pay']
-        self.remote_work = data['constraints']['remote']
+        # Student enrollment information
+        self.programming_languages = []
+        self.interests = []
+        self.overall_comfort_level = 3 # On a scale of 1 - 5, an average derived from sentiment questions
+        self.available = False
 
-        # Match results
-        self.eligible_project_ids = []
+        # Student preferences for projects
+        self.preferred_projects = []
+        self.project_match = ""
+        self._score = 0
 
+    def add_application_data(self, application_data):
+        if application_data['Q4'].strip() == '' or application_data['Q2'].strip() == '':
+            return False
+        self.legal_name = (", ".join([application_data['Q4'].strip(), application_data['Q2'].strip()])).upper().strip()
+        self.gnumber = application_data['Q7'].replace("G", "")
+        self.degree = application_data['Q10']
+        self.visa_status = ' '.join([application_data['Q32'].upper().strip(), application_data['Q32_4_TEXT'].upper().strip()])
+        try:
+            if "More than" in application_data['Q11']:
+                self.credits_range = (91, math.inf)
+            else:
+                self.credits_range = (
+                    int(application_data['Q11'].strip().replace("-", " ").split(" ")[0]),
+                    int(application_data['Q11'].strip().replace("-", " ").split(" ")[1])
+                    )
+            self.gpa_range = (
+                float(application_data['Q17'].split(" ")[0]),
+                float(application_data['Q17'].split(" ")[2])
+            )
+        except ValueError as e:
+            # print("No credits/GPA for student: {}".format(self.legal_name))
+            pass
 
-    def location_ok(self, project):
+        self.graduation_year = application_data['Q12'].split(" ")[0]
+        self.majors = list(set(application_data['Q14'].split(",") + [application_data['Q14_7_TEXT']] + [application_data['Q15']] + [application_data['Q15_12_TEXT']]))
+        if "Other" in self.majors:
+            self.majors.remove("Other")
+        if "Other declared major; please specify:" in self.majors:
+            self.majors.remove("Other declared major; please specify:")
+        if "" in self.majors:
+            self.majors.remove("")
+        try:
+            self.num_cs_courses = int(application_data['Q18'])
+        except:
+            self.num_cs_courses = 0
+        self.experience = True if application_data['Q20'] == 'Yes' else False
+        self.residency_status = application_data['Q32'] # One of: {US Citizen, permanent resident, F1 or J1, Other}
+        self.commitment = True if application_data['Q29'].strip() != '' else False
+        self.eligibility = True if application_data['Q39'].strip() != '' else False
+        self.score()
         return True
 
-    def pay_ok(self, project):
-        return True
+    def print_student_info(self):
+        print("------------ STUDENT {} ------------".format(self.id))
+        print("Legal name: {}".format(self.legal_name))
+        print("G-Number: {}".format(self.gnumber))
+        print("Degree: {}".format(self.degree))
+        print("Credits: {}".format(self.credits_range))
+        print("Graduation year: {}".format(self.graduation_year))
+        print("Majors: {}".format(self.majors))
+        print("GPA range: {}".format(self.gpa_range))
+        print("Number of CS courses taken: {}".format(self.num_cs_courses))
+        print("Previous experience at computing internship: {}".format(self.experience))
+        print("Overall comfort level: {}".format(self.overall_comfort_level))
+        print("Employer preferences: {}".format(self.preferred_projects))
 
-    def remote_work_ok(self, project):
-        return True
+    def add_enrollment_data(self, enrollment_data):
+        comfort_levels = [
+            SENTIMENT_TO_VALUE[enrollment_data["Q11"].strip().lower()],
+            SENTIMENT_TO_VALUE[enrollment_data["Q12"].strip().lower()],
+            6 - SENTIMENT_TO_VALUE[enrollment_data["Q13"].strip().lower()],
+            SENTIMENT_TO_VALUE[enrollment_data["Q14"].strip().lower()],
+            SENTIMENT_TO_VALUE[enrollment_data["Q16"].strip().lower()],
+            SENTIMENT_TO_VALUE[enrollment_data["Q17"].strip().lower()],
+            SENTIMENT_TO_VALUE[enrollment_data["Q18"].strip().lower()]
+        ]
+        self.overall_comfort_level = floor(float(sum(comfort_levels)) / len(comfort_levels))
+        self.available = True if len(enrollment_data["Q22"]) != 0 else False
+        self.score()
 
-    def get_eligible(self, projects):
-        for project in projects:
-            evaluation = [
-                self.location_ok(project),
-                self.pay_ok(project),
-                self.remote_work_ok(project)
-            ]
-            if evaluation == [True, True, True]:
-                self.eligible_project_ids.append(project.id)
+    def add_preferences(self, preference_data):
+        self.preferred_projects = preference_data['Preferences'].split(",")
+        self.score()
+
+    def score(self):
+        ''' A measure of how much priority a student should be given to
+            be matched to their preferences (to break ties) '''
+
+        total_points = len(SCORING_CRITERIA["STUDENT-SCORING"].keys())
+        points = 0
+        if "degrees" in SCORING_CRITERIA["STUDENT-SCORING"] and self.degree in SCORING_CRITERIA["STUDENT-SCORING"]["degrees"]:
+            points += 1
+        if "credits" in SCORING_CRITERIA["STUDENT-SCORING"] and self.credits_range[0] >= SCORING_CRITERIA["STUDENT-SCORING"]["credits"]["min"]:
+            points += 1
+        if "graduation_year" in SCORING_CRITERIA["STUDENT-SCORING"] and self.graduation_year in SCORING_CRITERIA["STUDENT-SCORING"]["graduation_year"]:
+            points += 1
+        if "major" in SCORING_CRITERIA["STUDENT-SCORING"] and True in (major in SCORING_CRITERIA["STUDENT-SCORING"]["major"] for major in self.majors):
+            points += 1
+        if "gpa" in SCORING_CRITERIA["STUDENT-SCORING"] and self.gpa_range[0] >= SCORING_CRITERIA["STUDENT-SCORING"]["gpa"]["min"]:
+            points += 1
+        if "number_cs_courses" in SCORING_CRITERIA["STUDENT-SCORING"] and self.num_cs_courses in range(SCORING_CRITERIA["STUDENT-SCORING"]["number_cs_courses"]["min"], SCORING_CRITERIA["STUDENT-SCORING"]["number_cs_courses"]["max"] + 1):
+            points += 1
+        if "experience" in SCORING_CRITERIA["STUDENT-SCORING"] and self.experience == SCORING_CRITERIA["STUDENT-SCORING"]["experience"]:
+            points += 1
+        return float(points) / total_points
+
+    def add_match(self, project):
+        self.project_match = project.organization
+
+    def matched(self):
+        return self.project_match != ""
--- src/generate_data.py
+++ src/generate_data.py
@@ -1,6 +1,8 @@
 import random
 import csv
 import string
+from entity_classes import Student
+
 
 qualtrics_fields = ['9/1/2020  9:17:00 PM', '9/1/2020  9:18:00 PM', 'Survey Preview', '*******', '100', '63', 'TRUE',
                     '9/1/2020  9:18:00 PM']
@@ -14,31 +16,131 @@ agreement_levels = ['Strongly disagree', 'Somewhat disagree', 'Neither agree nor
 likelihood_levels = ['Not at all likely', 'A little likely', 'Somewhat likely', 'Likely', 'Very Likely']
 pronouns = ['she/her/hers', 'he/his/him', 'they/them/their', 'ze/zir/zis']
 
-with open(r'./../data/student_enrollment.csv', 'a', newline='') as f:
-    writer = csv.writer(f)
 
-    for i in range(300):
-        row = []
-        response_id = 'R_' + ''.join(random.choices(string.ascii_letters + string.digits, k=15))
-        row.extend(qualtrics_fields)
-        row.append(response_id)
-        first_name = ''.join(random.choices(string.ascii_letters, k=10))
-        last_name = ''.join(random.choices(string.ascii_letters, k=10))
-        row.extend(['Person', 'BTT'])
-        row.append('BTTPerson@gmu.edu')
-        row.extend(['******', '53.1234', '18.1234', 'preview', 'EN', '23701129', first_name[0] + last_name + '@gmu.edu'])
-        row.extend([random.choice(pronouns), '', 'No', 'No', 'No'])
-        num_languages = random.randint(0, 6)
-        if num_languages > 0:
-            row.extend([random.choices(languages, k=num_languages), ''])
-        else:
-            row.extend(['None', ''])
-        row.extend([random.choices(technical_areas, k=random.randint(1, 7)), ''])
-        row.extend([random.choice(agreement_levels), random.choice(agreement_levels), random.choice(agreement_levels),
-                    random.choice(agreement_levels)])
-        row.extend(
-            [random.choice(likelihood_levels), random.choice(likelihood_levels), random.choice(likelihood_levels)])
-        row.append(first_name + '.' + last_name + '@gmail.com')
-        row.extend(['Point OfContact', 'pointofcontact@gmail.com', '789-456-1230', '123-456-7890'])
-        row.extend([first_name + ' ' + last_name, first_name + ' ' + last_name, ''])
-        writer.writerow(row)
+def get_students():
+    students = []
+    student_apps = open('./data/student_applications.csv', newline='')
+    reader = csv.DictReader(student_apps)
+    line_num = 0
+    for row in reader:
+        if line_num < 2:
+            line_num += 1
+            continue
+        if row['Q4'].strip() == '' and row['Q2'].strip() == '':
+            continue
+        name = (", ".join([row['Q4'].strip(), row['Q2'].strip()])).upper().strip()
+        if name.replace(", ", "").strip() != '':
+            students.append(name)
+        line_num += 1
+    return students
+
+
+def generate_student_enrollment_data():
+    with open(r'./data/student_enrollment.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        students = get_students()
+        header = ['StartDate','EndDate','Status','IPAddress','Progress','Duration (in seconds)','Finished','RecordedDate','ResponseId','RecipientLastName','RecipientFirstName','RecipientEmail','ExternalReference','LocationLatitude','LocationLongitude','DistributionChannel','UserLanguage','EMPLID','Q3','Q4','Q4_5_TEXT','Q5','Q6','Q7','Programming Languages','Q8_15_TEXT','Interests','Q9_10_TEXT','Q11','Q12','Q13','Q14','Q16','Q17','Q18','Q19','Q20_1','Q20_2','Q20_3','Q20_4','Q21','Q22','Q23']
+        writer.writerow(header)
+        for i in range(len(students)):
+            row = []
+            response_id = 'R_' + ''.join(random.choices(string.ascii_letters + string.digits, k=15))
+            row.extend(qualtrics_fields)
+            row.append(response_id)
+            first_name = students[i].split(", ")[1]
+            last_name = students[i].split(", ")[0]
+            row.extend([last_name, first_name])
+            row.append(first_name[0] + last_name + '@gmu.edu')
+            row.extend(['******', '53.1234', '18.1234', 'preview', 'EN', '23701129', first_name[0] + last_name + '@gmu.edu'])
+            row.extend([random.choice(pronouns), '', 'No', 'No', 'No'])
+            num_languages = random.randint(0, 6)
+            if num_languages > 0:
+                row.extend([random.choices(languages, k=num_languages), ''])
+            else:
+                row.extend(['None', ''])
+            row.extend([random.choices(technical_areas, k=random.randint(1, 7)), ''])
+            row.extend([random.choice(agreement_levels), random.choice(agreement_levels), random.choice(agreement_levels),
+                        random.choice(agreement_levels)])
+            row.extend(
+                [random.choice(likelihood_levels), random.choice(likelihood_levels), random.choice(likelihood_levels)])
+            row.append(first_name + '.' + last_name + '@gmail.com')
+            row.extend(['Point OfContact', 'pointofcontact@gmail.com', '789-456-1230', '123-456-7890'])
+            row.extend([first_name + ' ' + last_name, first_name + ' ' + last_name, ''])
+            writer.writerow(row)
+        f.close()
+
+def generate_student_preference_data(num_companies):
+    companies = []
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:num_companies]
+    for i in range(len(alphabet)):
+        companies.append("Company {}".format(alphabet[i]))
+    with open(r'./data/student_pref.csv', 'a', newline='') as f:
+        print("Writing student_pref.csv...")
+        students = get_students()
+        writer = csv.writer(f)
+        header = ['StartDate','EndDate','Status','IPAddress','Progress','Duration (in seconds)','Finished','RecordedDate','ResponseId','RecipientLastName','RecipientFirstName','RecipientEmail','ExternalReference','LocationLatitude','LocationLongitude','DistributionChannel','UserLanguage','First','Last','EMPLID','Email','Preferences','Q6_0_42_RANK','Q6_0_43_RANK','Q6_0_44_RANK','Q6_0_45_RANK','Q6_0_46_RANK','Q6_0_47_RANK','Q6_0_48_RANK','Q6_0_49_RANK','Q6_0_50_RANK','Q6_0_51_RANK']
+        writer.writerow(header)
+        for i in range(len(students)):
+            row = []
+            response_id = 'R_' + ''.join(random.choices(string.ascii_letters + string.digits, k=15))
+            row.extend(qualtrics_fields)
+            row.append(response_id)
+            first_name = students[i].split(", ")[1]
+            last_name = students[i].split(", ")[0]
+            row.extend([last_name, first_name])
+            row.append(first_name[0] + last_name + '@gmu.edu')
+            row.extend(['******', '53.1234', '18.1234', 'preview', 'EN', first_name, last_name, '23701129', first_name[0] + last_name + '@gmu.edu'])
+
+            random.shuffle(companies)
+            rankings = companies[:random.choice(range(5, 11))]
+            prefs = ",".join(rankings)
+            row.append(prefs)
+            row.extend(['', '', '', '', '', '', '', '', '', ''])
+            writer.writerow(row)
+
+        f.close()
+
+def generate_company_preference_data(num_companies):
+    companies = []
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:num_companies]
+    for i in range(len(alphabet)):
+        companies.append("Company {}".format(alphabet[i]))
+    students = get_students()
+
+    with open(r'./data/company_pref.csv', 'a', newline='') as f:
+        print("Writing company_pref.csv...")
+        header = ['V1','V2','V3','V4','V5','Q1','Q2','Q3','Organization','Prefer','Exclude']
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerow(header)
+        writer.writerow(header)
+        for company in companies:
+            row = []
+            row.extend(['R_2zJc453NsdZgPPU', 'Default Response Set', '9/1/2020 20:11', '9/1/2020 20:19', '1', '1', 'Zahin Faruque', 'zfaruqu@gmu.edu', company])
+            random.shuffle(students)
+            idx = random.choice(range(1, 20))
+            prefs = " / ".join(students[:idx])
+            exclude = " / ".join(students[idx:random.choice(range(1, 20))])
+            row.append(prefs)
+            row.append(exclude)
+            writer.writerow(row)
+        f.close()
+
+def generate_company_info_data(num_companies, max_student_capacity):
+    companies = []
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:num_companies]
+    for i in range(len(alphabet)):
+        companies.append("Company {}".format(alphabet[i]))
+    with open(r'./data/company_info.csv', 'a', newline='') as f:
+        print("Writing company_info.csv...")
+        header = ['Organization', 'Number of Students', 'Sponsored', 'F1/J1']
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerow(header)
+        for company in companies:
+            writer.writerow([
+                company,
+                random.choice(range(1, max_student_capacity + 1)),
+                random.choice(['True', 'False']),
+                random.choice(['True', 'False'])
+                ])
+        f.close()
